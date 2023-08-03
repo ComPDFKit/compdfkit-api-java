199f60553cce8ebef5b5abfffd79707af6f3fcba
wangpenghui <wangpenghui@kdanmobile.com>
2023-08-03 10:40:45 +0800

    update sample

--- sample/src/main/java/com/compdfkit/AddWatermark.java
+++ sample/src/main/java/com/compdfkit/AddWatermark.java
@@ -89,17 +89,18 @@ public class AddWatermark {
         String filePassword = "";
         CPDFAddWatermarkParameter fileParameter = new CPDFAddWatermarkParameter();
         fileParameter.setType("image");
-        fileParameter.setScale("1");
+        fileParameter.setScale("0.5");
         fileParameter.setOpacity("0.5");
-        fileParameter.setRotation("0.785");
+        fileParameter.setRotation("45");
         fileParameter.setTargetPages("1-2");
         fileParameter.setVertalign("center");
         fileParameter.setHorizalign("left");
-        fileParameter.setXoffset("100");
-        fileParameter.setYoffset("100");
+        fileParameter.setXoffset("50");
+        fileParameter.setYoffset("50");
         fileParameter.setFullScreen("1");
-        fileParameter.setHorizontalSpace("10");
-        fileParameter.setVerticalSpace("10");
+        fileParameter.setHorizontalSpace("100");
+        fileParameter.setVerticalSpace("100");
+        fileParameter.setFront("1");
         CPDFUploadFileResult uploadFileResult = client.uploadFile(file,taskId,fileParameter,new File("sample/test.jpg"));
         String fileKey = uploadFileResult.getFileKey();
         // perform tasks
